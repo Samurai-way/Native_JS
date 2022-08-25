eed874d27a98f3c6a08053a5be993e29994967d3
Samurai-way <olegmoishevych@gmail.com>
2022-08-25 21:16:23 +0300

    homeWork

--- script.js
+++ script.js
@@ -908,3 +908,129 @@ let tasks = {
 // }
 //
 // console.log(spongMame('Hello my friends'))
+
+const students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 90,
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 19,
+        isMarried: false,
+        scores: 100
+    },
+    {
+        name: "Helen",
+        age: 20,
+        isMarried: false,
+        scores: 110
+    },
+    {
+        name: "Ann",
+        age: 20,
+        isMarried: false,
+        scores: 105
+    },
+];
+
+const user = {
+    name: "Bob",
+    age: 23,
+    friends: ["Alex", "Nick", "John"]
+}
+
+//1. Создайте поверхностную копию объекта user
+let copyUser = {...user};
+console.log(user===copyUser);
+console.log(user.friends===copyUser.friends)
+
+//2. Полная (глубокая) копия объекта user
+let deepCopyUser = {...user, friends: [...user.friends]};
+// console.log(user===deepCopyUser)
+// console.log(user.friends===deepCopyUser.friends)
+
+//3. Поверхностная копия массива students
+let copyStudents = [...students];
+
+// console.log(copyStudents === students)
+// console.log(copyStudents[0] === students[0])
+// console.log(copyStudents[0].age === students[0].age)
+// console.log(copyStudents[1].name === students[1].name)
+
+//4*. Полная (глубокая) копия массива students (map)
+let deepCopyStudents = students.map(el => {...el})
+// console.log(deepCopyStudents === students)
+
+
+
+
+//5. Отсортируйте копию массива deepCopyStudents по алфавиту (sort)
+let sortedByName = deepCopyStudents.sort()
+// console.log(sortedByName);
+
+//6. Сформируйте массив студентов, у которых 100 и более баллов (filter)
+let  bestStudents = students.filter(t => t.scores >=100);
+// console.log(bestStudents)
+
+//6a. Получите массив ("вырежьте") трёх лучших студентов из массива deepCopyStudents (splice)
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+let topStudents = students.slice(0,2);
+// console.log(topStudents)
+// console.log(deepCopyStudents)
+
+//7. Сформируйте массив холостых студентов (filter)
+let notMarriedStudents = students.filter(s => s.isMarried === false);
+// console.log(notMarriedStudents)
+
+//8. Сформируйте массив имён студентов (map)
+let studentsNames = students.map(s => s.name);
+// console.log(studentsNames)
+
+//8a. Сформируйте строку из имён студентов, разделённых
+// - пробелом (join)
+// - запятой (join)
+let namesWithSpace = students.join(' ')
+// console.log(namesWithSpace)      *****
+
+let namesWithComma = students.join(',');
+// console.log(namesWithComma)       *****
+
+//9. Добавьте всем студентам свойство "isStudent" со значением true (map)
+let trueStudents = students.map((e) => {return {isStudent: true}});
+// console.log(trueStudents)
+
+//10. Nick женился. Выполните соответствующие преобразование массива students (map)
+let studentsWithMarriedNick = students.map(e => e.name === 'Nick');
+// console.log(studentsWithMarriedNick) ***
+
+//11. Найдите студента по имени Ann (find)
+let ann = students.find(a=>a.name === 'Ann');
+// console.log(ann)
+
+//12. Найдите студента с самым высоким баллом (reduce)
+// - c помощью reduce
+// - *не испльзуя методы массивов и Math.max()*
+let bestStudent = students.find(a => a.scores > 110);
+// console.log(bestStudent) ***
+
+//13. Найдите сумму баллов всех студентов (reduce)
+
+let sum = students.reduce()
+// console.log(sum)
+
