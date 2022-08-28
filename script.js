f2bd1f2008ba873c9b29794f6dbc39b059fb9753
Samurai-way <olegmoishevych@gmail.com>
2022-08-28 09:45:50 +0300

    codewars task 2

--- script.js
+++ script.js
@@ -1315,3 +1315,17 @@ let tasks = {
 //
 // it("between(1, 4)", () => assert.deepStrictEqual(between(1, 4), [1, 2, 3, 4]));
 // it("between(-2, 2)", () => assert.deepStrictEqual(between(-2, 2), [-2, -1, 0, 1, 2]));
+
+
+function getGrade (s1, s2, s3) {
+    return {
+        10: 'A',
+        9: 'A',
+        8: 'B',
+        7: 'C',
+        6: 'D'
+    }[Math.floor((s1 + s2 + s3) / 30)] || 'F'
+
+}
+
+console.log(getGrade(80, 85, 82))
