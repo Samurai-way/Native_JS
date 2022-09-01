f8a2ec28101b36b780474de178570a7084eff1f9
Samurai-way <olegmoishevych@gmail.com>
2022-09-01 20:58:54 +0300

    native js

--- script.js
+++ script.js
@@ -1546,3 +1546,110 @@ let tasks = {
 // let abbrevName =(name) => name.split(" ").map(el => el[0].toUpperCase()).join(".")
 //
 // console.log(abbrevName('Petia Vasia'))
+
+//1. Реализуйте функцию, которая принимает параметром подсторку,
+// число повторов и разделитель, а возвращает сторку, состоящую
+// из указанного количества повторов подстроки с использованием разделителя.
+// repeatString("yo", 3, " ") => "yo yo yo"
+// repeatString("yo", 3, ",") => "yo,yo,yo"
+// for или str.repeat()
+
+let func = (str, number, strDEF) => {
+    return JSON.stringify(str.repeat(number).split(strDEF))
+}
+
+// console.log(func('yo', 4, ' '))
+
+//2. Реализуйте функцию, которая принимает параметром
+// сторку и подстроку, а возвращает true,
+// если строка начинается с указанной подстроки,
+// в противном случае - false. Регистр не учитывается.
+
+// checkStart("Incubator", "inc") => true
+// checkStart("Incubator", "yo") => false
+// str.startWith() slice indexOF
+
+let task2 = function(value, set){
+    return value.startsWith(set)
+}
+// console.log(task2("Incubator", "tor"))
+
+//3. Реализуйте функцию, которая принимает
+// параметром строку и число (количество символов),
+// а возвращает строку из параметров, обрезанную до
+// указанного количества символов и завершает её многоточием.
+
+
+//truncateString("Всем студентам инкубатора желаю удачи!", 10) => "Всем студе..."
+
+function kitcut( text, limit) {
+    text = text.trim();
+    if( text.length <= limit) return text;
+    text = text.slice( 0, limit); // тупо отрезать по лимиту
+    lastSpace = text.lastIndexOf(" ");
+    if( lastSpace > 0) { // нашлась граница слов, ещё укорачиваем
+        text = text.substr(0, lastSpace);
+    }
+    return text + "...";
+}
+// console.log(kitcut("Всем студентам инкубатора желаю удачи!", 5))
+
+//4. Реализуйте функцию, которая принимает параметром сторку (предложение)
+// и возвращает самое короткое слово в предложении, если в параметрах пустая
+// строка, то возвращает null.
+// getMinLengthWord("Всем студентам инкубатора желаю удачи!") => "Всем"
+// getMinLengthWord("") => null
+// split()
+
+function findLongestWord(str) {
+    return str.split(' ').reduce((a, b) => (b.length < a.length) ? b : a);
+};
+
+// console.log(findLongestWord("Всем студентам инкубатора желаю у"))
+
+//5. Реализуйте функцию, которая принимает параметром сторку (предложение)
+// и возвращает то же предложение, где все слова написаны строчными, но начинаются с заглавных букв.
+// setUpperCase("всем стУдентам инкуБатора Желаю удачИ!") => "Всем Студентам Инкубатора Желаю Удачи!"
+
+function capitalize(str) {
+    let x = "";
+    for (i = 0; i < str.length; i++) {
+        if (str[i - 1] === " " || i === 0) {
+            x += str[i].toUpperCase();
+        } else {
+            x += str[i];
+        }
+    }
+    return x;
+}
+
+// console.log(capitalize("всем стУдентам инкуБатора Желаю удачИ!"))
+
+//6. Реализуйте функцию, котрая принимает параметрами строку и подстроку. Если все
+// символы подстроки содержаться в стороке - возвращает true, если нет -
+// возвращает false. Проверка проводится без учёта регистра и без учётом
+// повторяющихся символов.
+//* попробовать учитывать повтор символов в подстроке
+
+// isIncludes("Incubator", "Cut") => true
+// isIncludes("Incubator", "table") => false
+// isIncludes("Incubator", "inbba") => true
+// isIncludes("Incubator", "inba") => true
+// isIncludes("Incubator", "Incubatorrr")=> true
+
+function inc(str1, str2){
+    let a = str1.toLowerCase().split('')
+    let b = str2.toLowerCase().split('')
+
+    while(b.length) {
+        if(a.includes(b[0])){
+            a.splice(a.indexOf(b[0], 1))
+            b.splice(0,1)
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+// console.log(inc("Incubator", "Cut"))
