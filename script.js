66fab92f89b7d5b30f5bbc5f50038b19e13caa0d
Samurai-way <olegmoishevych@gmail.com>
2022-09-18 10:34:49 +0300

    Associative massive

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-function getSum( a,b ) {
-    let min = Math.min(a,b),
-        max = Math.max(a,b)
-    return (max - min + 1) * (max + min) / 2
-}
+                                            //      7 kyu
 
-console.log(getSum(0, 1))
-// assert.strictEqual(getSum(0,-1),-1);
-// assert.strictEqual(getSum(0,1),1);
+// function getSum( a,b ) {
+//     let min = Math.min(a,b),
+//         max = Math.max(a,b)
+//     return (max - min + 1) * (max + min) / 2
+// }
+//
+// console.log(getSum(0, 1))
+// // assert.strictEqual(getSum(0,-1),-1);
+// // assert.strictEqual(getSum(0,1),1);
+
+                                            // Repeat
+
+// let person = {
+//     name: 'Dima',
+//     age: 15,
+//     info:[
+//         {name: 'Kiev', street: 'Soborna'}
+//     ]
+// }
+// let copy = {
+//     ...person,
+//     info: person.info.map(e => ({...e}))
+// }
+//
+// copy.info[0].street = 'Green'
+// console.log(copy)
+// console.log(person)
+                                    // Associatove massive
+let arr = {
+    'one': 1,
+    'two': 2,
+    'isDone': true,
+    'name': 'Dima'
+}
+let b = {
+    ...arr,
+    'isDone': 15
+}
+console.log(b)
+
+let person = {
+    'name': {
+        'city': 'Kiev',
+        'street': 'Soborna'
+    },
+    'street':{
+        name: 'Soborna',
+        'age': 20
+    }
+}
+person.name.street = 'Kharkov'
+person.sorname = 'Ivanov'
+console.log(person)
