a7bfe7ae5f7ef77164783306d2d6567216ed8afd
Samurai-way <olegmoishevych@gmail.com>
2022-09-25 11:07:52 +0300

    codewars 7 kyu

--- script.js
+++ script.js
@@ -203,8 +203,24 @@ function getMiddle(s) {
     return a.join('')
 }
 
-console.log(getMiddle("testing"))
+// console.log(getMiddle("testing"))
 // Test.assertEquals(getMiddle("test"),"es");
 // Test.assertEquals(getMiddle("testing"),"t");
 // Test.assertEquals(getMiddle("middle"),"dd");
 // Test.assertEquals(getMiddle("A"),"A");
+
+function accum(s) {
+   return s.toLowerCase().
+   split('').
+   map((el, index)=>el.
+   toUpperCase() + el.repeat(index)).
+   join('-')
+}
+
+console.log(accum("ZpglnRxqenU"))
+
+// Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+// Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+// Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+// Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+// Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
