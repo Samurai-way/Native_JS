d447c19af92d158f6b887d3a82e13d449e589058
Samurai-way <olegmoishevych@gmail.com>
2022-10-04 23:02:34 +0300

    codewars 7 kyu (flat, sort)

--- script.js
+++ script.js
@@ -507,3 +507,15 @@ function solve(s){
 // Test.assertEquals(solve("CODe"),"CODE");
 // Test.assertEquals(solve("COde"),"code");
 // Test.assertEquals(solve("Code"),"code");
+
+function flattenAndSort(array) {
+    return array.flat().sort((a,b) => a - b)
+}
+
+// console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]))
+
+// assert.deepEqual(flattenAndSort([]), []);
+// assert.deepEqual(flattenAndSort([[], []]), []);
+// assert.deepEqual(flattenAndSort([[], [1]]), [1]);
+// assert.deepEqual(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+// assert.deepEqual(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]), [1, 2, 3, 4, 5, 6, 100]);
