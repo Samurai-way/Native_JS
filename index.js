d1fba944e3f974076ce69816947a622f20797a2a
Samurai-way <olegmoishevych@gmail.com>
2022-10-26 18:07:08 +0300

    native js

--- index.js
+++ index.js
@@ -173,30 +173,180 @@
 
 //true, true
 
+// setTimeout(()=>{
+//     console.log(1)
+// },30)
+//
+// setTimeout(()=>{
+//     console.log(2)
+// },10)
+//
+// new Promise((res) => {
+//     console.log(3)
+//     res()
+//     console.log(4)
+// }).then(()=>{
+//     console.log(5)
+// })
+//
+// async function test1(){
+//     console.log(6)
+//     await test2()
+//     console.log(9)
+// }
+//
+// async function test2(){
+//     console.log(8)
+// }
+// test1()
 
-setTimeout(()=>{
-    console.log(1)
-},30)
+// console.log(
+//     {} + {}
+// )
 
-setTimeout(()=>{
-    console.log(2)
-},10)
+// console.log(
+//     {} + 'hello'
+// )
+//
+// console.log(
+//     20 / {}
+// )
 
-new Promise((res) => {
-    console.log(3)
-    res()
-    console.log(4)
-}).then(()=>{
-    console.log(5)
+// const user = {
+//     name: 'Alex',
+//     age: 12,
+//     [Symbol.toPrimitive]: function (hint) {
+//         console.log(hint)
+//     }
+// }
+//
+// console.log(user + 5)
+
+// const a = Symbol('Dima')
+// console.log(a)
+
+// const rect = {
+//     x: 6,
+//     y: 10,
+//     [Symbol.toPrimitive]: function (hint) {
+//         console.log(hint)
+//         if (hint === 'number') {
+//             return this.x * this.y
+//         }
+//         return 'Object'
+//     }
+// }
+//
+// const rect2 = {
+//     x: 2,
+//     y: 5,
+//     [Symbol.toPrimitive]: function (hint) {
+//         console.log(hint)
+//         if (hint === 'number') {
+//             return this.x * this.y
+//         }
+//         return 'Object'
+//     }
+// }
+//
+// console.log(rect > rect2 ? 'yes' : 'no')
+
+
+// const user = {
+//     age: 'alex',
+//     toString(){
+//         return 'hello'
+//     },
+//     valueOf(){
+//         console.log('hello')
+//         return 5
+//     }
+// }
+//
+// // console.log(user * 10)
+//
+// // user * 10
+//
+// // String(user)
+//
+// console.log(String(user))
+
+// console.log(
+//     ({}) / 5
+// )
+
+// console.log(
+//     // [1,2,3].toString()
+//     [{},{}].toString()
+// )
+
+// console.log(
+//     {} + 1
+// )
+//
+// const a = {}
+// const b = {}
+// a[b] = 10
+//
+// console.log(a)
+
+// let a = '10'
+// console.log(+a)
+
+// const nums = [1,2,3]
+//
+// const iterator = nums[String.iterator]
+// // console.log(iterator)
+//
+// const nums = [1,2,3]
+//
+// while (true){
+//     const current = iterator.next()
+//     if(current.done) break
+//     console.log(current.value)
+// }
+// const foo = (str)=>{
+//     let arr = []
+//     for (const arrElement of str) {
+//         arr.push(arrElement)
+//     }
+//     return arr
+// }
+// console.log(foo('hello'))
+
+// console.log(
+// Array.from([1, 2, 3])
+// )
+// const user = {
+//     name: 'Alex'
+// }
+// user.age = 10
+// delete user.name
+// delete user.age
+// console.log(user)
+//
+// const user = {
+//     name: 'Alex'
+// }
+//
+// const user1 = Object.create(Object.prototype,{
+//     name:'Dima',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true
+// })
+//
+// console.log(user1)
+
+const user = {
+    name: 10
+}
+
+const user2 = {}
+
+Object.defineProperties(user2, 'age', {
+    first: 'one',
+    second: 'two'
 })
 
-async function test1(){
-    console.log(6)
-    await test2()
-    console.log(9)
-}
-
-async function test2(){
-    console.log(8)
-}
-test1()
+console.log(user2)
