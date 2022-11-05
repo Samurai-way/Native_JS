da8806a049170f6e5d70345e819b22c65a7574cc
Samurai-way <olegmoishevych@gmail.com>
2022-11-05 20:52:37 +0200

    const trueName = /^oleg+@mail\.ru$/

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const regex = /358\d{7}/;
 const regexText = /@gmail.\w{3}/;
 const emailName = /^[a-z]+@gmail\.com$/ // начало от a до z, а конец @gmail.com и только точка и ничего другого
 const trueRes = /^\w+@gmail\.com$/
+const trueName = /^oleg+@mail\.ru$/
 
 
+console.log(trueName.test('oleg@mail.ru'))
 // console.log(regex.test(358111111))
 // console.log(regexText.test('oleg@gmail.cm'))
 // console.log(emailName.test('aassasaz@gmail,com'))
-console.log(trueRes.test('oleg@gmail.com'))
+// console.log(trueRes.test('oleg@gmail.com'))
