e03b7fc40033c4e2e62bcdf1936e4442cfb34d08
Samurai-way <olegmoishevych@gmail.com>
2022-11-10 19:08:36 +0200

    замыкание

--- promise.js
+++ promise.js
@@ -1279,3 +1279,23 @@
 // }}}}}}}
 // console.log(a)
 
+// for (let i = 0; i < 10; i++) {}
+// console.log(i)
+
+//
+// for (let j = 0; j < 10; j++) {}
+//
+// console.log(j)
+
+const counterCreator = () => {
+    let count = 0
+
+    return ()=>{
+        console.log(++count)
+    }
+}
+const counter = counterCreator()
+
+counter()
+counter()
+counter()
