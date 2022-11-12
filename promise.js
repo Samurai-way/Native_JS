fe1f5e85399c3da87a5a9df000485ac86796b485
Samurai-way <olegmoishevych@gmail.com>
2022-11-12 22:52:50 +0200

    docs: proto, prototype

--- promise.js
+++ promise.js
@@ -1,34 +1,37 @@
-// let a = {}
-//
-// let b = {}
-//
-// console.log(a.__proto__ === b.__proto__)
 
-// let users = []
-// let cars = []
-//
-// console.log(users.__proto__ === cars.__proto__)
 
-let promise = new Promise(()=>{})
 
-console.log(promise.__proto__ === Promise.prototype)
 
-let number = 10
 
-console.log(number.__proto__ === Number.prototype)
 
-let str = 'string'
+let m = {
+    n: 10
+}
+console.log(m.prototype) // false
+console.log(m.__proto__ === Object.prototype) // true
+console.log(m.__proto__ === Object.prototype) // true
 
-console.log(str.__proto__ === String.prototype)
+let num = [1,2,3,4]
+console.log(num.prototype)// false
+console.log(num.__proto__ === Array.prototype)// true
+console.log(num.__proto__ === Array.prototype)// true
 
-function f() {}
+true
 
-console.log(f.__proto__ === Function.prototype)
+class Class {
 
-class Class {}
+}
 
-console.log(Class.__proto__ === Function.prototype)
+console.log(Class.__proto__ === Function.prototype) // true
 
-let boolean = false
+false
 
-console.log(boolean.__proto__ === Boolean.prototype)
+let shogun = new Class()
+let shogu1 = new Class()
+console.log(shogun.__proto__ === Class.prototype) // true
+console.log(shogun.__proto__ === shogu1.__proto__) // true
+
+
+
+
+
