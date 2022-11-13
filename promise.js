f6572751e292624d7032f624bd604dfc1168746b
Samurai-way <olegmoishevych@gmail.com>
2022-11-13 21:19:35 +0200

    learn: this, and other

--- promise.js
+++ promise.js
@@ -1,21 +1,83 @@
-
-
-// function f() {
-//     console.log(this.name)
+// foo()
+//
+// function foo() {
+//     bar()
+//     {
+//         function bar() {
+//             console.log('bar')
+//         }
+//     }
 // }
-// f()
+//
+// let a = () => {
+//     console.log('a')
+// }
+//
+// const run = (cb) => {
+//     setTimeout(cb, 1000)
+// }
+//
+// a = () => {
+//     console.log('new a')
+// }
 
-let man = {
-    name: 'Dima',
-    age: 10,
-    hey(){
-        const hello = () =>{
+// const a = {
+//     mag: 'Hello world',
+//     getMag(){
+//         const msg = 'hello'
+//         console.log(this.mag)
+//     }
+// }
+//
+// a.getMag()
+
+// this.name = 'global'
+//
+// const a = {
+//     name: 'a',
+//     logName: () => {
+//         console.log(this.name)
+//     }
+// }
+//
+// setTimeout(a.logName, 100)
+
+// const a = {
+//     name: 'a',
+//     getName(){
+//         console.log(this.name)
+//     }
+// }
+//
+// const b = {
+//     name: 'b'
+// }
+//
+// const c = {
+//     name: 'c'
+// }
+//
+// a.getName()
+// const foo = a.getName.bind(b)
+// a.getName()
+// a.getName.call(b)
+// foo.call(c)
+
+
+const a = {
+    name: 'a'
+}
+const b = {
+    name: 'b',
+    hi(){
+        (()=>{
             console.log(this.name)
-        }
-       return  hello
+        }).call(a)
     }
 }
 
-let hop = man.hey()
-console.log(hop)
+const c = {
+    name: 'c'
+}
 
+b.hi.call(c)
