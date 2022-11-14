58aa66702e1b12f20de0bf9cae976cfb20451b17
Samurai-way <olegmoishevych@gmail.com>
2022-11-14 09:27:12 +0200

    learn: this, and other

--- promise.js
+++ promise.js
@@ -62,22 +62,142 @@
 // a.getName()
 // a.getName.call(b)
 // foo.call(c)
+//
+//
+// const a = {
+//     name: 'a'
+// }
+// const b = {
+//     name: 'b',
+//     hi(){
+//         (()=>{
+//             console.log(this.name)
+//         }).call(a)
+//     }
+// }
+//
+// const c = {
+//     name: 'c'
+// }
+//
+// b.hi.call(c)
+//
+// const a = Object.create({}, {})
+// const b = {}
+//
+// b[a] = 'a'
+// console.log(b)
+
+// Object.prototype.name = 'Object'
+//
+// const a = {
+//     getName(){
+//         console.log(this.name)
+//     }
+// }
+//
+// const b = {
+//     name: 'b'
+// }
+//
+// a.getName()
+//
+// b.__proto__ = a
+//
+// b.getName()
+//
+// const c = Object.create(a,{
+//     name: {
+//         value: 'c'
+//     }
+// })
+//
+// c.getName()
+
+// const user = {
+//     sleep(){
+//         this.isSleeping = true
+//     }
+// }
+//
+// const john = {}
+//
+// john.__proto__ = user
+// john.sleep()
+//
+// console.log(john.isSleeping)
+
+// console.log(Object.__proto__ === Function.prototype)
+//
+// const arr = []
+// console.log(arr.constructor === Array)
+//
+// const a = () => {
+//     console.log(a.__proto__.__proto__.constructor === Object)
+// }
+//
+// arr.__proto__ = []
+// console.log(arr.__proto__.__proto__ === Object.prototype)
+//
+// class BaseUser {}
+//
+// class Class extends BaseUser{
+//     constructor(name) {
+//         super();
+//         this.name = name
+//     }
+//     getName(){
+//         return this.name
+//     }
+// }
+//
+// const user = new Class('Dimas')
+//
+// console.log(user.__proto__ === Class.prototype)
+// console.log(typeof Class)
+// console.log(Class === Class.prototype.constructor)
+// console.log(Class.__proto__ === Function.prototype)
+
+// class Promise {
+//     getName(){
+//         return this.name
+//     }
+//
+// }
+//
+// const b = Object.create(Promise,{
+//     name: {
+//         value: 'b'
+//     }
+// })
+//
+// console.log(b.getName())
+//
+// class Class {
+//     constructor(name) {
+//         this.name = name
+//         if(!name){
+//             return {name: 'No test name'}
+//         }
+//     }
+// }
+//
+// const t = new Class()
+// const tl = new Class('Should return undefined')
+//
+// console.log(t.__proto__ === tl.__proto__)
 
 
-const a = {
-    name: 'a'
-}
-const b = {
-    name: 'b',
-    hi(){
-        (()=>{
-            console.log(this.name)
-        }).call(a)
+function Test() {
+    this.name = name
+    if(!name){
+        return {name: 'No test name!'}
     }
 }
 
-const c = {
-    name: 'c'
-}
+const t = new Test()
 
-b.hi.call(c)
+const tl = new Test('Should return undefined!')
+
+console.log(t.__proto__ === tl.__proto__)
+
