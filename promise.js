49e475d7131970cef2b403818118a8e420b1d7b8
Samurai-way <olegmoishevych@gmail.com>
2022-11-17 12:10:06 +0200

    learn: Native JS exm

--- promise.js
+++ promise.js
@@ -1025,3 +1025,114 @@
 // console.log('d')
 
 // a b d Promise 1, Promise 3, timer 0, Promise 2, timer 10, timer 20
+
+// console.log('1' + true)
+// console.log([1] + true)
+// console.log('2' * 3 + true)
+
+// 1true,
+// 1true,
+// 7
+
+// console.log(null == undefined)
+// console.log(null === null)
+// console.log({} + {})
+
+// true, true, Object, Object
+
+// const a = {
+//     name: 'a'
+// }
+//
+// let b = {
+//     name: 'b'
+// }
+//
+// let c = {
+//     name: 'c'
+// }
+//
+// c = b
+// b = a
+//
+// c.name = 'new c'
+// b.name = 'new b'
+//
+// console.log(a.name)
+// console.log(b.name)
+// console.log(c.name)
+
+// const b = {
+//     name: 'b',
+//     getName: function () {
+//         return (()=> this.name)()
+//     }
+// }
+//
+// const c = {
+//     name: 'c'
+// }
+//
+// Object.setPrototypeOf(c,b)
+// console.log(c.getName())
+
+// const a = {
+//     age: 25
+// }
+//
+// const b = {
+//     age: 23,
+//
+//     hi: () => {
+//         console.log(this.age)
+//     },
+//
+//     hi2(){
+//         (()=>{
+//             console.log(this.age)
+//         }).call(a)
+//     }
+// }
+//
+// b.hi()
+// b.hi2()
+//
+// Object.prototype.name = 'Object'
+//
+// Object.prototype.showName = function () {
+//     console.log(this.name)
+// }
+//
+// const a = {
+//     name: 'a'
+// }
+//
+// const b = Object.create(a)
+// b.showName()
+//
+// Object.setPrototypeOf(b, {})
+//
+// b.showName()
+//
+// const arr = []
+// console.log(arr.constructor === Array)
+//
+// const a = () => {}
+// console.log(a.__proto__.__proto__.constructor === Object)
+//
+// arr.__proto__ = []
+// console.log(arr.__proto__.__proto__ === Object.prototype)
+//
+// let i
+// for ( i = 0; i < 10; i++) {
+//     setTimeout(()=>{
+//         console.log(i)
+//     },0)
+// }
+
+//a, b, d, Prom1, Prom3, timer0, Prom2, timer10, timer20
+
+// console.log('1' + true)
+// console.log([1] + true)
+// console.log('2' * 3 + true)
+//1true, 1true, 7
