72c12ac7810a0130fa622735e7ad5f4f8818d1d6
Samurai-way <olegmoishevych@gmail.com>
2022-11-18 23:04:45 +0200

    learn: Native JS setPrototypeOf, create, prototype

--- promise.js
+++ promise.js
@@ -1410,18 +1410,167 @@
 // b.hi()
 // b.hi2()
 
-Object.prototype.name = 'Object'
+// Object.prototype.name = 'Object'
+//
+// Object.prototype.showName = function () {
+//     console.log(this.name)
+// }
+//
+// const a = {
+//     name: 'a'
+// }
+//
+// const b = Object.create(a)
+// b.showName()
+//
+// Object.setPrototypeOf(b, {})
+// b.showName()
 
-Object.prototype.showName = function () {
-    console.log(this.name)
-}
+// const arr = []
+// console.log(arr.constructor === Array)
+//
+// const a = () => {}
+// console.log(a.__proto__.__proto__.constructor === Object)
+//
+// arr.__proto__ = []
+// console.log(arr.__proto__.proto__ === Object.prototype)
 
-const a = {
-    name: 'a'
-}
+// console.log('a')
+// console.log('b')
+//
+// setTimeout(()=>{
+//     console.log('timer 10')
+// },10)
+//
+// setTimeout(()=>{
+//     console.log('timer 20')
+// },20)
+//
+// Promise.resolve('Promise 1').then((res)=> console.log(res))
+//
+// Promise.resolve(setTimeout(()=>(console.log('Prom2')),5))
+//
+// Promise.resolve('Prom3').then(console.log)
+//
+// setTimeout(()=>{
+//     console.log('timer 0')
+// },0)
+//
+// console.log('d')
 
-const b = Object.create(a)
-b.showName()
+//a, b, d, Pro1, Pro3, tim0, Pro2, tim10, tim20
 
-Object.setPrototypeOf(b, {})
-b.showName()
+// console.log('1'+true)
+// //1true
+//
+// console.log([1,2,3] + true)
+// //1,2,3true
+//
+// console.log('2' * 3 + true)
+// //7
+//
+// // console.log('2' + 3)
+// //23
+//
+// console.log(null == undefined)
+// console.log(null === null)
+// console.log({} + {})
+
+//true true obj obj
+
+// let a = {
+//     name: 'a'
+// }
+//
+// let b = {
+//     name: 'b'
+// }
+//
+// let c = {
+//     name: 'c'
+// }
+//
+// c = b
+// b = a
+//
+// c.name = 'new c'
+// b.name = 'new b'
+//
+// console.log(a.name)
+// console.log(b.name)
+// console.log(c.name)
+
+// const b = {
+//     name: 'b',
+//     getName: function () {
+//         return (
+//             () => this.name
+//         )()
+//     }
+// }
+//
+// const c = {
+//     name: 'c'
+// }
+//
+// Object.setPrototypeOf(c,b)
+//
+// console.log(
+//     c.getName()
+// )
+//
+// const a = {
+//     age: 24
+// }
+//
+// const b = {
+//     age: 21,
+//
+//
+//     hi: ()=>{
+//         console.log(this.age)
+//     },
+//
+//     hi2(){
+//         (()=>{
+//             console.log(this.age)
+//         }).call(a)
+//     }
+// }
+//
+// b.hi()
+// b.hi2()
+//
+// Object.prototype.name = 'Object'
+//
+// Object.prototype.showName = function () {
+//     console.log(this.name)
+// }
+//
+// const a = {
+//     name: 'a'
+// }
+//
+// const b = Object.create(a)
+// b.showName()
+//
+// Object.setPrototypeOf(b, {})
+// b.showName()
+//
+// const arr = []
+// console.log(arr.constructor === Array)
+//
+// const a = () => {}
+// console.log(a.__proto__.__proto__.constructor === Object)
+//
+// arr.__proto__ = []
+// console.log(arr.__proto__.__proto__ === Object.prototype)
+//
+// let i;
+// for(i = 0; i < 10; i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },1)
+// }
+
+
