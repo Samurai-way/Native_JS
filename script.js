2bc51e996558e8b09a9e2056f1d0599dd716f4ff
Samurai-way <olegmoishevych@gmail.com>
2022-12-15 18:21:23 +0200

    learn: Native JS: this, call, apply, bind

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-console.log(this)
+// !use strict ==> object
+// use strict ==> any data type
+
+
+// 1. Global Scope ==> global object window
+
+// console.log(this)
+//
+// const user = {
+//     name: 'Alex',
+//     showName(){
+//         console.log(this.name)
+//     }
+// }
+// user.showName()
+
+                                     //============= INSIDE FUNCTION =========
+
+function foo() {
+    console.log(this)
+}
+foo()
 
 const user = {
-    name: 'Alex',
-    showName(){
-        console.log(this.name)
-    }
+    showFoo: foo
 }
-user.showName()
+user.showFoo() // 2
+
+foo.bind()()
+foo.call()
+foo.apply()
