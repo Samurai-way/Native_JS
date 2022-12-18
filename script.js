119e89677cebe3158b5372a77cb6c9b3643b596d
Samurai-way <olegmoishevych@gmail.com>
2022-12-18 12:10:18 +0200

    learn: Native JS: examples

--- script.js
+++ script.js
@@ -2,6 +2,19 @@
 
 const a = false && 'true'
 console.log(a)
-let a,b;
-console.log(b)
+// let a,b;
+// console.log(b)
 
+                                                            // let a = {
+                                                            //     name: 'a',
+                                                            // }
+                                                            //
+                                                            // let b = {
+                                                            //     name: 'b'
+                                                            // }
+                                                            //
+                                                            // let c = {
+                                                            //     name: 'c'
+                                                            // }
+                                                            //
+                                                            // console.log(g)
